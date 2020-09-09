d85c4c4740c5f92230ea82aa6bdad7b658ebcc4b
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-09-09 19:32:10 -0300

    feat: organiza pastas

---
+++ src/com/study/java/atividade01/Main.java
@@ -0,0 +1,11 @@
+package com.study.java.atividade01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Pessoa pessoa = new Pessoa();
+    pessoa.dadosPessoais();
+
+    }
+}
---
+++ src/com/study/java/atividade01/Pessoa.java
@@ -0,0 +1,48 @@
+package com.study.java.atividade01;
+
+public class Pessoa {
+
+    private String nome;
+    private Integer idade;
+
+    private final Double valorFerias = 1.33;
+
+    public void dadosPessoais() {
+
+        Estagiario estagiario = new Estagiario("Maria Eduarda", 19);
+
+        System.out.println("Nome: " + estagiario.getNome());
+        System.out.println("Idade: " + estagiario.getIdade());
+        System.out.println("Instituição: " + estagiario.getInstituicao());
+        System.out.println(estagiario.getSalarioFerias(estagiario.getSalario()));
+
+        Gerente gerente = new Gerente("Odilon", 24);
+
+        System.out.println("Nome: " + gerente.getNome());
+        System.out.println("Idade: " + gerente.getIdade());
+        System.out.println("Endereço: " + gerente.getEndereco());
+        System.out.println(gerente.getSalarioFerias(gerente.getSalario()));
+    }
+
+    public double getSalarioFerias(Double salario) {
+        return (salario * valorFerias);
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
